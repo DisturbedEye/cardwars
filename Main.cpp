853228092241827af6f0a6ce328470928bf498af
иля <44808471+DisturbedEye@users.noreply.github.com>
2022-04-04 21:03:24 +0700

    добавил меню настроек
    добавил только кнопку back с функционалом назад, reset to defaults, save и переключатели разрешений без функционала

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Engine.hpp"
+#include <string>
 
 std::ostream &operator<<(std::ostream &out, const sf::Vector2f &v)
 {
@@ -79,13 +80,12 @@ int main_menu(sf::RenderWindow &window)
 	Vector2f bsize = Vector2f(200.f, 60.f); // button size
 	Vector2f bhalfsize = bsize / 2.f; // half of button size
 	sf::Font &font = loadFont();
-	// buttons 
-
+	// buttons
 	Button<Rect> start = engine::Button<Rect>(Rect(bsize), "Start", font, resolution.y / 27u);
 	Button<Rect> settings = engine::Button<Rect>(Rect(bsize), "Settings", font, resolution.y / 27u);
 	Button<Rect> about_us = engine::Button<Rect>(Rect(bsize), "About Us", font, resolution.y / 27u);
 	Button<Rect> exit = engine::Button<Rect>(Rect(bsize), "Exit", font, resolution.y / 27u);
-	std::vector<engine::Button<sf::RectangleShape>> buttons; // buttons too 
+	std::vector<engine::Button<sf::RectangleShape>> buttons; // buttons too
 	buttons.push_back(start);	 // 1
 	buttons.push_back(settings); // 2
 	buttons.push_back(about_us); // 3
@@ -130,26 +130,102 @@ void game_settings(sf::RenderWindow &window)
 	* audio (later add)
 	* other settings
 	*/
+
 	using engine::Button;
 	using sf::Vector2f;
 	using engine::math::mix;
 	using namespace parametrs;
 	typedef sf::RectangleShape Rect;
+	sf::Mouse m;
+	Vector2f mpos;
+	Vector2f res = Vector2f(float(resolution.x), float(resolution.y));
 	std::vector<Vector2f> resolutions = { {2560, 1440}, {1920, 1080}, {1600, 900}, {1366, 768}, {1280, 720}, {800, 600} };
-	Vector2f ssize = Vector2f(60, 60); // switchers size
-	Vector2f tsize = Vector2f(400, 60); // rtarget size
-	Button<Rect> switcherL = Button<Rect>(Rect(ssize));
-	Button<Rect> switcherR = Button<Rect>(Rect(ssize));
-	Button<Rect> rscreen = Button<Rect>(Rect(tsize));
-	std::vector<Button<Rect>> buttons = {switcherL, rscreen, switcherR};
+
+	Vector2f bpos = Vector2f(res.x / 6, res.y /3) / 2.f; // button position
+	Vector2f ssize = Vector2f(60.f, 60.f); // switchers size
+	Vector2f tsize = Vector2f(400.f, 60.f); // rtarget size
+	Vector2f bsize = Vector2f(200.f, 60.f); // button size
+	Vector2f rsize = Vector2f(350.f, 60.f); // reset button size
+	Vector2f bhalfsize = bsize / 2.f; // half of button size
+	sf::Color bcolor = sf::Color(115, 1, 4); // button color (reddish for me plz)
 	auto &font = loadFont();
+
+	// buttons
+	Button<Rect> backB = Button<Rect>(Rect(bsize), "Back", font, resolution.y / 27u);
+	Button<Rect> resetB = Button<Rect>(Rect(bsize), "Save", font, resolution.y / 27u);
+	Button<Rect> saveB = Button<Rect>(Rect(rsize), "Reset to defaults", font, resolution.y / 27u);
+	Button<Rect> switcherL = Button<Rect>(Rect(ssize), "<", font, resolution.y / 27u);
+	Button<Rect> switcherR = Button<Rect>(Rect(ssize), ">", font, resolution.y / 27u);
+	std::string ressx;
+	std::string ressy;
+	ressx = std::to_string(resolution.x);
+	ressy = std::to_string(resolution.y);
+	Button<Rect> rScreen = Button<Rect>(Rect(tsize), ressx+" x "+ressy, font, resolution.y / 27u); // resolution display through button
+
+
+	//totally not ripped off from main menu surely
+	std::vector<engine::Button<sf::RectangleShape>> buttons;
+	buttons.push_back(backB);	  // 1
+	buttons.push_back(saveB);     // 2
+	buttons.push_back(resetB);    // 3
+	buttons.push_back(switcherL); // 4
+	buttons.push_back(rScreen);   // 5
+	buttons.push_back(switcherR); // 6
+
+    uint8_t j, k;
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 			if (event.type == sf::Event::Closed)
 				window.close();
+		mpos = Vector2f(m.getPosition(window));
 		window.clear();
+		//code
+		k = 1;
+		j = 0;
+		for (auto &b : buttons){
+            if (b.isIntersected(mpos) && k!=5)
+                b.shape.setFillColor(mix(bcolor, sf::Color::White));
+            else b.shape.setFillColor(bcolor);
+            //button positioning
+            if (k == 1){
+                b.setPosition(Vector2f(0, 0));
+            }
+            if (k >= 4 && k <=6){
+                b.setPosition(Vector2f((bpos.x + (j++) * (float(resolution.y)/24.f+ssize.x)), bpos.y));
+                if (k == 6){
+                    b.setPosition(Vector2f((bpos.x + (j++) * (float(resolution.y)/24.f+ssize.x)+tsize.x/1.75), bpos.y));
+                }
+            }
+            if (k >=2 && k < 4){
+                b.setPosition(Vector2f((bpos.x + (j++) * (float(resolution.y)/24.f+rsize.x)), res.y-bpos.y));
+            }
+            if (b.isPressed(sf::Mouse::Button::Left, mpos)){
+                // button functions
+                switch (k){
+                    // back
+                    case 1: return;
+                    // save
+                    case 2:{
+
+                    }
+                    // reset
+                    case 3:{
+
+                    }
+                    // etc
+                    case 4:{
+
+                    }
+                    case 6:{
+
+                    }
+                }
+            }
+            k++;
+            window.draw(b);
+		}
 		window.display();
 	}
 }
@@ -209,6 +285,7 @@ void about_us(sf::RenderWindow &window)
 				window.close();
 		mpos = Vector2f(m.getPosition(window));
 		window.clear();
+		//code
 		if (back.isIntersected(mpos))
 			back.shape.setFillColor(mix(bcolor, sf::Color::White));
 		else back.shape.setFillColor(bcolor);
